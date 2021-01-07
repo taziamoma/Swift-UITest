8f635ec3c75c025c7ea91434447b47ff1c6d187d
Taziamoma Abraham <taziamoma@gmail.com>
2021-01-07 14:18:46 -0600

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Taziamoma Abraham on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
